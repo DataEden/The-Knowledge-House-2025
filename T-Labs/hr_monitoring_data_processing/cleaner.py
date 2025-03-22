14d8c596ca53b631e1c6f37c64325c7ea8120e63
DataEden <160447213+DataEden@users.noreply.github.com>
2025-03-21 23:54:44 -0400

    Updated Standar deviation(), using variance function.

--- T-Labs/hr_monitoring_data_processing/cleaner.py
+++ T-Labs/hr_monitoring_data_processing/cleaner.py
@@ -11,13 +11,15 @@ def filter_nondigits(data: list) -> list:
         list: A list of strings of type int elements.          
     """    
     
-    clean_list = []  # define and Initialize empty list to store permissible elements.
-    for element in data:  # Iterate over list of strings.
-        # remove any \n, \t, " ", etc.., from string(s).
-        clean_element = element.strip()
+    if not data:
+        return []
+  
+    clean_list = []  # Initialize empty list to store permissible elements.
+    for val in data:  # Iterate over list of strings.
+        # remove newline characters from the string.
+        clean_element = val.strip()
         # Check if element is a digit.        
         if clean_element.isdigit():  
-            clean_list.append(int(element))  # Convert string to permissible element and append to list.
+            clean_list.append(int(val))  # Convert string to permissible element  and append list.
     return clean_list  # Return list of permissible elements.
-
-
+    
