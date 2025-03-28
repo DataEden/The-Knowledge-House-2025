da80f702dcd1bd92568e9f5fee27911680e56c10
DataEden <160447213+DataEden@users.noreply.github.com>
2025-03-28 01:53:17 -0400

    Update comments.

--- T-Labs/hr_monitoring_data_processing/main.py
+++ T-Labs/hr_monitoring_data_processing/main.py
@@ -2,10 +2,11 @@
 The main Python mpdule that combines cleaner and metrics functions in order to
 perform comprehensive analysis
 """
-
+ # Import packages/functions from external scripts.
 from metrics import average, maximum, standard_deviation
 from cleaner import filter_nondigits
 
+ # Import Python module
 import matplotlib.pyplot as plt
 
 
@@ -22,15 +23,15 @@ def run(filename: str) -> None:
     """ 
     data = []
 
-    # open file using file I/O and read it into the `data` list
+    # open file using file I/O and read it into the `data` list.
     with open(filename, "r") as file:
         data = file.readlines()
 
-    # Use `filter_nondigits` to clean the data and remove invalid entries, save the output to a new variable
+    # Use `filter_nondigits` to clean the data and remove invalid entries, then saves the output to a new variable.
     clean_data = filter_nondigits(data)
-    
 
-    # plot this data to explore changes in heart rate for visualization to the "images" folder
+
+    # plot this data to explore changes in heart rate for visualization to the "images" folder.
     plt.plot(clean_data)
     plt.xlabel("Time")
     plt.ylabel("Heart beat / rate")
@@ -39,14 +40,15 @@ def run(filename: str) -> None:
     plt.savefig("images/hr_data.png")
     plt.close()
     
-    # calculate the average, maximum, and standard deviation functions 
+    # calculate the average, maximum, and standard deviation functions. 
+    # call each function from metrics.py and save the output to variables.
     avg_hr = round(average(clean_data), 2)
     max_hr = round(maximum(clean_data), 2)
     std_dev_hr = round(standard_deviation(clean_data), 2)
 
-    # return all 3 values
+    # return all 3 values.
     return avg_hr, max_hr, std_dev_hr
 
-
+    # Display output to the screen.
 if __name__ == "__main__":
     print(run("data/phase1.txt"))
