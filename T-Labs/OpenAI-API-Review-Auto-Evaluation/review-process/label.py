af7f62c07f19e408155fba777f109da33e892600
DataEden <160447213+DataEden@users.noreply.github.com>
2025-04-02 16:38:58 -0400

    Added get_sentiment function.

--- T-Labs/OpenAI-API-Review-Auto-Evaluation/review-process/label.py
+++ T-Labs/OpenAI-API-Review-Auto-Evaluation/review-process/label.py
@@ -1,19 +1,39 @@
 from openai import OpenAI
-
+import json
 
 def get_sentiment(text: list) -> list:
     """
     INSERT DOCSTRING HERE
     """
-    system_prompt = """
-    ...
-    """
+    # Check if the input is a list of strings
+    # Validate the input
+    if not isinstance(text, list) or not all(isinstance(item, str) for item in text):
+        return "Wrong input. text must be an array of strings."
+    # Check if the list is empty
+    if not text:
+       return "Wrong input. text must be an array of strings."
+    
+    client = OpenAI()
 
-    prompt = f"""
+    #reviews = json.loads(text)
+    
+    system_prompt = f"""
     For each line of text in the string below, please categorize the review
     as either positive, neutral, negative, or irrelevant.
 
     Use only a one-word response per line. Do not include any numbers.
-    {text}
-    """
-    pass
+        """
+    user_prompt = "Here are the product reviews:\n" + "\n".join(text)
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages = [
+            {"role": "developer", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ]
+    )
+    text_response = completion.choices[0].message.content
+
+    return [line.strip().lower() for line in text_response.splitlines()]
+
+
