64f60c73ccc697243de3dffa90c41b215ab049f2
DataEden <160447213+DataEden@users.noreply.github.com>
2025-04-03 01:47:52 -0400

    Updated calling funtion, docstring, and return statement

--- T-Labs/OpenAI-API-Review-Auto-Evaluation/review-process/label.py
+++ T-Labs/OpenAI-API-Review-Auto-Evaluation/review-process/label.py
@@ -2,14 +2,14 @@ from openai import OpenAI
 
 def get_sentiment(text: list) -> list:
     """
-    INSERT DOCSTRING HERE
+    
     """
     # Check if the input is a list of strings
     if not isinstance(text, list) or not all(isinstance(item, str) for item in text):
         return "Wrong input. text must be an array of strings."
     # Check if the list is empty
     if not text:
-       return "Wrong input. text must be an array of strings."
+        return "Wrong input. text must be an array of strings."
     
     client = OpenAI()
 
@@ -25,11 +25,14 @@ def get_sentiment(text: list) -> list:
         model="gpt-4o-mini",
         messages = [
             {"role": "developer", "content": system_prompt},
-            {"role": "user", "content": user_prompt}
+            {"role": "user", "content": user_prompt},
+            
         ]
+        
     )
     text_response = completion.choices[0].message.content
 
-    return [line.strip().lower() for line in text_response.splitlines()]
+    return text_response(text) #[line.strip().lower() for line in text_response.splitlines()]
+
 
 
